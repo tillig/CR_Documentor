24bfdde0965c4cd35d4f7bdbcfbd8f05fc6741f4
travis.illig <travis.illig@e29d1091-9d51-0410-bb39-710e510d8bfb>
2008-08-05 14:48:38 +0000

    Created branchpoint for starting task.

---
+++ branches/issue/11/tags/source/branchpoint/DocTest/Tags/Inline/C.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace DocTest.Tags.Inline {
+	/// <summary>
+	/// This is <c>inline code</c> in a class summary.
+	/// </summary>
+	public class C {
+		/// <summary>
+		/// This is <c>inline code</c> in a method summary.
+		/// </summary>
+		public void Method(){
+		}
+	}
+}
