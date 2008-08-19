cc09523d2c2b3a6fed7021774ae61b6954bd2f3f
travis.illig <travis.illig@e29d1091-9d51-0410-bb39-710e510d8bfb>
2008-08-19 16:29:27 +0000

    Created branchpoint for issue 13 codeline.

---
+++ branches/issue/13/tags/source/branchpoint/DocTest/Tags/TopLevel/Returns.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace DocTest.Tags.TopLevel
+{
+	/// <summary>
+	/// This class tests the 'returns' tag.
+	/// </summary>
+	public class Returns
+	{
+		/// <summary>
+		/// This method returns a string.
+		/// </summary>
+		/// <returns>Empty string is the return value.</returns>
+		public string Method()
+		{
+			return "";
+		}
+	}
+}
