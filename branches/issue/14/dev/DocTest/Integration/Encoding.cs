2e98e69f7701426ecba5cc1358899a4529a34502
travis.illig <travis.illig@e29d1091-9d51-0410-bb39-710e510d8bfb>
2008-09-03 22:01:49 +0000

    Added an Encoding test class.

---
+++ branches/issue/14/dev/DocTest/Integration/Encoding.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace DocTest.Integration
+{
+	/// <summary>
+	/// Class used for testing encoding/display issues.
+	/// </summary>
+	public class Encoding
+	{
+		/// <summary>
+		/// This method illustrates different HTML encoding issues.
+		/// </summary>
+		/// <remarks>
+		/// <para>
+		/// Standard text tests:
+		/// </para>
+		/// <para>
+		/// Encoded characters that should display as literals:
+		/// &lt; &apos; &quot; &amp; &gt;
+		/// </para>
+		/// <para>
+		/// Unencoded characters that are optionally encoded: " '
+		/// </para>
+		/// <para>
+		/// Code block tests:
+		/// </para>
+		/// <code>
+		/// Encoded characters that should display as literals:
+		/// &lt; &apos; &quot; &amp; &gt;
+		/// </code>
+		/// <code>
+		/// Unencoded characters that are optionally encoded: " '
+		/// </code>
+		/// </remarks>
+		public void HtmlEncoding()
+		{
+		}
+	}
+}
