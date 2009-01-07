c0ab1a122d99d317c7f544f54f3e8a733a9157a7
travis.illig <travis.illig@e29d1091-9d51-0410-bb39-710e510d8bfb>
2009-01-07 20:59:12 +0000

    Added State enumeration and RunState property.

--- branches/spike/internal-server-threading-update/dev/Plugin/Server/WebServer.cs
+++ branches/spike/internal-server-threading-update/dev/Plugin/Server/WebServer.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading;
 
 namespace CR_Documentor.Server
 {
@@ -7,7 +10,39 @@ namespace CR_Documentor.Server
 	/// </summary>
 	public class WebServer : IDisposable
 	{
+		/// <summary>
+		/// Enumeration of possible run states for the web server.
+		/// </summary>
+		public enum State
+		{
+			/// <summary>
+			/// The server is stopped.
+			/// </summary>
+			Stopped,
+
+			/// <summary>
+			/// The server is in the process of starting or stopping.
+			/// </summary>
+			Intermediate,
+
+			/// <summary>
+			/// The server is started.
+			/// </summary>
+			Started
+		}
+
 		private WebListener _listener = null;
+		private Queue<HttpListenerContext> _requestQueue = new Queue<HttpListenerContext>();
+		private long _runState = (long)State.Stopped;
+
+		/// <summary>
+		/// Gets or sets the content to serve to the browser.
+		/// </summary>
+		/// <value>
+		/// A <see cref="System.String"/> with the HTML to serve to the
+		/// browser on incoming requests.
+		/// </value>
+		public virtual string Content { get; set; }
 
 		/// <summary>
 		/// Gets the port that the server was initialized with.
@@ -16,6 +51,21 @@ namespace CR_Documentor.Server
 		/// A <see cref="System.UInt16"/> with the port this server is listening on.
 		/// </value>
 		public virtual UInt16 Port { get; private set; }
+
+		/// <summary>
+		/// Gets the current run state of the server.
+		/// </summary>
+		/// <value>
+		/// A <see cref="CR_Documentor.Server.WebServer.State"/> indicating the current
+		/// run state of the server.
+		/// </value>
+		public State RunState
+		{
+			get
+			{
+				return (State)Interlocked.Read(ref _runState);
+			}
+		}
 
 		/// <summary>
 		/// Gets the unique identifier for this server.
@@ -41,17 +91,11 @@ namespace CR_Documentor.Server
 		}
 
 		/// <summary>
-		/// Gets or sets the content to serve to the browser.
-		/// </summary>
-		/// <value>
-		/// A <see cref="System.String"/> with the HTML to serve to the
-		/// browser on incoming requests.
-		/// </value>
-		public virtual string Content { get; set; }
-
-		/// <summary>
 		/// Initializes a new instance of the <see cref="WebServer"/> class.
 		/// </summary>
+		/// <param name="port">
+		/// The port that should be listened to for incoming requests.
+		/// </param>
 		/// <exception cref="System.NotSupportedException">
 		/// The <see cref="System.Net.HttpListener"/> is not supported on this
 		/// operating system.
