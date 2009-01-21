7b53ebd70a17bffb7a0a35b5db080eb8a9213f9f
travis.illig <travis.illig@e29d1091-9d51-0410-bb39-710e510d8bfb>
2009-01-21 20:48:10 +0000

    Updated version to 2.2.1.0 for release.

--- trunk/AssemblyVersion.vb
+++ trunk/AssemblyVersion.vb
@@ -3,4 +3,4 @@ Imports System.Reflection
 
 <Assembly: AssemblyProduct("CR_Documentor")> 
 <Assembly: AssemblyCopyright("Copyright (c) 2004 CR_Documentor Contributors")> 
-<Assembly: AssemblyVersion("2.2.0.0")> 
+<Assembly: AssemblyVersion("2.2.1.0")> 
