f35ca42da6849249a989784c3a4a2601797f2263
travis.illig <travis.illig@e29d1091-9d51-0410-bb39-710e510d8bfb>
2009-01-21 00:55:10 +0000

    Created codeline for issue 8.

---
+++ branches/issue/8/dev/AssemblyVersion.vb
@@ -0,0 +1,6 @@
+Imports System
+Imports System.Reflection
+
+<Assembly: AssemblyProduct("CR_Documentor")> 
+<Assembly: AssemblyCopyright("Copyright (c) 2004 CR_Documentor Contributors")> 
+<Assembly: AssemblyVersion("2.2.0.0")> 
