7c83847211d57cbf078297dd0dab11467e67623b
travis.illig <travis.illig@e29d1091-9d51-0410-bb39-710e510d8bfb>
2009-01-21 20:50:02 +0000

    Created release tag for version 2.2.1.0.

---
+++ tags/release/2.2.1.0/AssemblyVersion.cs
@@ -0,0 +1,6 @@
+using System;
+using System.Reflection;
+
+[assembly: AssemblyProduct("CR_Documentor")]
+[assembly: AssemblyCopyright("Copyright (c) 2004 CR_Documentor Contributors")]
+[assembly: AssemblyVersion("2.2.1.0")]
