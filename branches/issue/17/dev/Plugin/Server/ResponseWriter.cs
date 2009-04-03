991008fa32b2acd00cebb3abcbf0073b0f8512ad
travis.illig <travis.illig@e29d1091-9d51-0410-bb39-710e510d8bfb>
2009-04-03 23:55:13 +0000

    Created codeline for issue 17.

---
+++ branches/issue/17/dev/Plugin/Server/ResponseWriter.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Net;
+using System.Text;
+
+namespace CR_Documentor.Server
+{
+	/// <summary>
+	/// Utility class for issuing responses to web requests.
+	/// </summary>
+	public static class ResponseWriter
+	{
+		/// <summary>
+		/// Writes an HTML response.
+		/// </summary>
+		/// <param name="context">The incoming request context.</param>
+		/// <param name="html">The HTML to respond with.</param>
+		/// <exception cref="System.ArgumentNullException">
+		/// Thrown if <paramref name="context" /> is <see langword="null" />.
+		/// </exception>
+		public static void WriteHtml(HttpListenerContext context, string html)
+		{
+			if (context == null)
+			{
+				throw new ArgumentNullException("context");
+			}
+			HttpListenerResponse response = context.Response;
+			string content = String.IsNullOrEmpty(html) ? "&nbsp;" : html;
+			byte[] buffer = Encoding.UTF8.GetBytes(content);
+			WriteSuccessResponse(response, buffer, Encoding.UTF8);
+		}
+
+		/// <summary>
+		/// Writes a successful response.
+		/// </summary>
+		/// <param name="response">The response object to write to.</param>
+		/// <param name="content">The content to send.</param>
+		/// <param name="encoding">The encoding the content is in.</param>
+		private static void WriteSuccessResponse(HttpListenerResponse response, byte[] content, Encoding encoding)
+		{
+			response.StatusCode = (int)HttpStatusCode.OK;
+			response.StatusDescription = "OK";
+			response.ContentLength64 = content.Length;
+			response.ContentEncoding = encoding;
+			response.OutputStream.Write(content, 0, content.Length);
+			response.OutputStream.Close();
+			response.Close();
+		}
+	}
+}
---
+++ branches/issue/17/dev/Plugin/Transformation/Syntax/Language.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace CR_Documentor.Transformation.Syntax
+{
+	/// <summary>
+	/// Contains language identifiers used in syntax preview generation.
+	/// </summary>
+	public static class Language
+	{
+		/// <summary>
+		/// Language identifier: C#
+		/// </summary>
+		public const string CSharp = "CSharp";
+
+		/// <summary>
+		/// Language identifier: C, C++
+		/// </summary>
+		public const string C = "C/C++";
+
+		/// <summary>
+		/// Language identifier: Visual Basic
+		/// </summary>
+		public const string Basic = "Basic";
+	}
+}
