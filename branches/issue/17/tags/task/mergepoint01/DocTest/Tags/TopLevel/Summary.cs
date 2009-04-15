93941c539b8453f98d64f0d27176e79d392c5620
travis.illig <travis.illig@e29d1091-9d51-0410-bb39-710e510d8bfb>
2009-04-15 21:11:46 +0000

    Created mergepoint for pushing new task changes into main codeline.

---
+++ branches/issue/17/tags/task/mergepoint01/DocTest/Tags/TopLevel/Summary.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace DocTest.Tags.TopLevel
+{
+	/// <summary>
+	/// This class tests the 'summary' tag.
+	/// </summary>
+	public class SummaryClass
+	{
+		/// <summary>
+		/// <para>
+		/// This is a method with a summary.
+		/// </para>
+		/// <para>
+		/// This is the second paragraph in the summary.
+		/// </para>
+		/// </summary>
+		public void Method()
+		{
+		}
+	}
+}
