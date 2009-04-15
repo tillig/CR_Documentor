193833780ce22983620487a4105bd32693d982d9
travis.illig <travis.illig@e29d1091-9d51-0410-bb39-710e510d8bfb>
2009-04-15 18:29:51 +0000

    Created mergepoint for pushing task changes into main codeline.

---
+++ branches/issue/17/tags/task/mergepoint00/DocTest/Tags/Inline/Paramref.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace DocTest.Tags.Inline {
+	/// <summary>
+	/// This class tests the 'paramref' tag.
+	/// </summary>
+	public class Paramref {
+		/// <summary>
+		/// This summary references the <paramref name="obj" /> parameter.
+		/// </summary>
+		/// <param name="obj">The parameter for this method.</param>
+		public void Method(object obj){
+		}
+	}
+}
