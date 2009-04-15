96dd6d07f9cdf2c67e7acbf1b574b8195cae3968
travis.illig <travis.illig@e29d1091-9d51-0410-bb39-710e510d8bfb>
2009-04-15 21:12:48 +0000

    Created release tag for version 2.3.0.0.

---
+++ tags/release/2.3.0.0/Plugin/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Security.Permissions;
+using CR_Documentor.ProductModule;
+using CR_Documentor.Properties;
+using DevExpress.CodeRush.Common;
+
+[assembly: DXCoreAssembly(DXCoreAssemblyType.PlugIn, ProductConstants.PlugInName)]
+[assembly: DXCoreProduct(typeof(CR_DocumentorProductModule))]
+[assembly: DXCoreAuthorizedAssembly(LoadAuthorization.AllProducts)]
+
+[assembly: ComVisible(false)]
+[assembly: SecurityPermission(SecurityAction.RequestMinimum)]
+
+[assembly: AssemblyTitle(ProductConstants.PlugInName)]
+[assembly: AssemblyDescription("DXCore plugin that provides the ability to preview how XML documentation comments will render.")]
