51df9f5cdef09b0ece316d9ccff5f647dd35f6f8
travis.illig <travis.illig@e29d1091-9d51-0410-bb39-710e510d8bfb>
2009-04-15 21:17:18 +0000

    Resolved issue #17 - Plugin doesn't display in DXCore 9.1.3 (changes between 9.1.2 and 9.1.3). Also includes update of version to 2.3.0.1 for release.

--- AssemblyVersion.cs
+++ AssemblyVersion.cs
@@ -3,4 +3,4 @@ using System.Reflection;
 
 [assembly: AssemblyProduct("CR_Documentor")]
 [assembly: AssemblyCopyright("Copyright (c) 2004 CR_Documentor Contributors")]
-[assembly: AssemblyVersion("2.3.0.0")]
+[assembly: AssemblyVersion("2.3.0.1")]
