e0047340db1169b41bee32676e422d7ac66fa4f5
travis.illig <travis.illig@e29d1091-9d51-0410-bb39-710e510d8bfb>
2009-04-17 18:18:31 +0000

    Created codeline for issue 19.

---
+++ branches/issue/19/dev/DocTest/Syntax/TestAbstractClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DocTest.Syntax
+{
+	/// <summary>
+	/// Abstract class used in testing syntax generation.
+	/// </summary>
+	public abstract class TestAbstractClass
+	{
+		/// <summary>
+		/// Abstract method member of an abstract class.
+		/// </summary>
+		/// <param name="parameter">Abstract method parameter.</param>
+		public abstract void AbstractMethod(string parameter);
+	}
+}
