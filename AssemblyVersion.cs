e6696c1971e6f47be7793681e4c2180a8639c065
travis.illig <travis.illig@e29d1091-9d51-0410-bb39-710e510d8bfb>
2009-04-24 16:51:06 +0000

    Resolved issue #19 - Plugin doesn't display in "About" box in DXCore 9.1.3.

--- AssemblyVersion.cs
+++ AssemblyVersion.cs
@@ -2,5 +2,5 @@ using System;
 using System.Reflection;
 
 [assembly: AssemblyProduct("CR_Documentor")]
-[assembly: AssemblyCopyright("Copyright (c) 2004 CR_Documentor Contributors")]
+[assembly: AssemblyCopyright("Copyright © 2004 CR_Documentor Contributors")]
 [assembly: AssemblyVersion("2.3.0.1")]
