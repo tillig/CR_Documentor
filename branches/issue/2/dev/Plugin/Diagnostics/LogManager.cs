aa35e2e097eb3e28ca675b022bbe5d5c50852a4a
travis.illig <travis.illig@e29d1091-9d51-0410-bb39-710e510d8bfb>
2009-05-06 00:03:14 +0000

    Implemented the LogManager for retrieving specific log implementations based on class type.

--- branches/issue/2/dev/Plugin/Diagnostics/LogManager.cs
+++ branches/issue/2/dev/Plugin/Diagnostics/LogManager.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace CR_Documentor.Diagnostics
 {
@@ -18,9 +16,24 @@ namespace CR_Documentor.Diagnostics
 		/// An <see cref="CR_Documentor.Diagnostics.ILog"/> that can be used for
 		/// writing messages to the event log.
 		/// </returns>
+		/// <remarks>
+		/// Items in the <see cref="n:CR_Documentor.ContextMenu"/> namespace will
+		/// log to the Menus log in DXCore; everything else logs to the ToolWindows log.
+		/// </remarks>
+		/// <exception cref="System.ArgumentNullException">
+		/// Thrown if <paramref name="logOwner" /> is <see langword="null" />.
+		/// </exception>
 		public static ILog GetLogger(Type logOwner)
 		{
-			throw new NotImplementedException();
+			if (logOwner == null)
+			{
+				throw new ArgumentNullException("logOwner");
+			}
+			if (logOwner.Namespace.StartsWith("CR_Documentor.ContextMenu"))
+			{
+				return new MenuLogger(logOwner);
+			}
+			return new ToolWindowLogger(logOwner);
 		}
 	}
 }
