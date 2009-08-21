f55764d498aaaa81f4edda6f8a079c3b0d5d1c67
travis.illig <travis.illig@e29d1091-9d51-0410-bb39-710e510d8bfb>
2009-08-21 18:44:07 +0000

    Created codeline for issue 7.

---
+++ branches/issue/7/dev/AssemblyVersion.vb
@@ -0,0 +1,6 @@
+Imports System
+Imports System.Reflection
+
+<Assembly: AssemblyProduct("CR_Documentor")> 
+<Assembly: AssemblyCopyright("Copyright © 2004 CR_Documentor Contributors")> 
+<Assembly: AssemblyVersion("2.3.0.1")> 
---
+++ branches/issue/7/dev/DocTestVB/Syntax/TestInterface.vb
@@ -0,0 +1,15 @@
+﻿Namespace Syntax
+
+	''' <summary>
+	''' Interface used in testing syntax generation.
+	''' </summary>
+	Public Interface TestInterface
+
+		''' <summary>
+		''' This is a member of the interface.
+		''' </summary>
+		Sub InterfaceMember()
+
+	End Interface
+
+End Namespace
