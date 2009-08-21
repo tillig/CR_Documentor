bbf4df49ea70d4da711b6537b81230015e034b2b
travis.illig <travis.illig@e29d1091-9d51-0410-bb39-710e510d8bfb>
2009-08-21 18:43:56 +0000

    Created branchpoint for issue 7 codeline.

---
+++ branches/issue/7/tags/source/branchpoint/DocTest/Tags/TopLevel/Include.cs
@@ -0,0 +1,31 @@
+using System;
+
+namespace DocTest.Tags.TopLevel
+{
+	/// <summary>
+	/// This class tests the 'include' tag. Tests are commented out to avoid build
+	/// warnings but can be uncommented as needed to test things real-time.
+	/// </summary>
+	public class IncludeClass
+	{
+		/// <summary>
+		/// Absolute include:
+		/// [
+		/// <!-- include file='C:\Documents and Settings\tillig\My Documents\Visual Studio 2008\Projects\CR_Documentor\dev\DocTest\Tags\TopLevel\include.xml' path='doc/members/member[@name="M:Absolute.Include"]/remarks/text()[position() = 1]'/ -->
+		/// ]
+		/// </summary>
+		public void AbsoluteInclude()
+		{
+		}
+
+		/// <summary>
+		/// Relative include:
+		/// [
+		/// <!-- include file='.\include.xml' path='doc/members/member[@name="M:Relative.Include"]/remarks/text()[position() = 1]'/ -->
+		/// ]
+		/// </summary>
+		public void RelativeInclude()
+		{
+		}
+	}
+}
