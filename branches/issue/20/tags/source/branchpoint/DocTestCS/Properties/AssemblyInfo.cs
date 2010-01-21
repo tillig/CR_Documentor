0e6dfc2a608af9675b89a26cc9708183ce328427
travis.illig <travis.illig@e29d1091-9d51-0410-bb39-710e510d8bfb>
2010-01-21 16:07:45 +0000

    Created branchpoint for issue 20 codeline.

---
+++ branches/issue/20/tags/source/branchpoint/DocTestCS/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Security.Permissions;
+
+[assembly: ComVisible(false)]
+[assembly: SecurityPermission(SecurityAction.RequestMinimum)]
+
+[assembly: AssemblyTitle("DocTestCS")]
+[assembly: AssemblyDescription("C# code for testing the XML doc comment preview.")]
