bb672942e547fa5947d520e07316fe5d4c311246
travis.illig <travis.illig@e29d1091-9d51-0410-bb39-710e510d8bfb>
2010-01-21 17:53:10 +0000

    Created mergepoint for pushing task changes into trunk.

---
+++ branches/issue/20/tags/task/mergepoint00/IntegrationTests/DocTestCS/Syntax/TestEnum.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Xml.Serialization;
+
+namespace DocTestCS.Syntax
+{
+	/// <summary>
+	/// Enum used in testing syntax generation.
+	/// </summary>
+	[Flags]
+	[XmlRoot("root", DataType = "string")]
+	public enum TestEnum : long
+	{
+		/// <summary>
+		/// First value.
+		/// </summary>
+		Value1,
+
+		/// <summary>
+		/// Second value.
+		/// </summary>
+		[XmlEnum]
+		Value2
+	}
+}
