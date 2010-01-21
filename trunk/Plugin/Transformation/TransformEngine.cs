a27a3258f90d2a1d48a50373724f59aa1a8f5678
travis.illig <travis.illig@e29d1091-9d51-0410-bb39-710e510d8bfb>
2010-01-21 18:24:42 +0000

    Resolved issue #20 - Updated the way documentation nodes are found when rendering class members so enum members, etc., with attributes will have their summary documentation found correctly. (Found problem with the first way it was fixed; fixed it a different way, this time hopefully correctly.)

--- trunk/Plugin/Transformation/TransformEngine.cs
+++ trunk/Plugin/Transformation/TransformEngine.cs
@@ -574,22 +574,14 @@ namespace CR_Documentor.Transformation
 		/// </remarks>
 		protected void RenderElementSummary(SP.LanguageElement element)
 		{
-			if (element == null || !element.HasDocument)
+			SP.CodeElement codeElement = element as SP.CodeElement;
+			if (codeElement == null || !codeElement.CanBeDocumented)
 			{
 				this.Writer.Write("&nbsp;");
 				return;
 			}
 
-			// Walk back node to node until we hit the comment. This is important
-			// in situations like enums where an enum member might also have an
-			// attribute we need to skip over before we get to the comment.
-			SP.LanguageElement commentNode = element.PreviousNode;
-			while (commentNode != null && !(commentNode is SP.XmlDocComment))
-			{
-				commentNode = commentNode.PreviousNode;
-			}
-
-			SP.XmlDocComment comment = CommentParser.GetXmlDocComment(commentNode);
+			SP.XmlDocComment comment = codeElement.DocComment;
 			if (comment != null)
 			{
 				System.Xml.XmlDocument document = CommentParser.ParseXmlCommentToXmlDocument(comment, "Parse error at line {0}, character {1} of source document.  XML Parse Error Message: {2}", "Error: {0}");
