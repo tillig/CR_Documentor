fbf2e88d1ff4cd53b6d34571b6a0109aa5f24087
travis.illig <travis.illig@e29d1091-9d51-0410-bb39-710e510d8bfb>
2010-01-21 17:50:50 +0000

    Created mergepoint for bringing trunk changes into task.

---
+++ branches/issue/20/tags/source/mergepoint00/Plugin/AssemblyExtensions.cs
@@ -0,0 +1,61 @@
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace CR_Documentor
+{
+	/// <summary>
+	/// Extension methods for <see cref="System.Reflection.Assembly"/>.
+	/// </summary>
+	public static class AssemblyExtensions
+	{
+		// TODO: When we target .NET 3.5, convert these to real extension methods.
+
+		/// <summary>
+		/// Reads an embedded resource string from an assembly.
+		/// </summary>
+		/// <param name="assembly">The <see cref="System.Reflection.Assembly"/> to read the resource from.</param>
+		/// <param name="resourcePath">The path to the embedded resource.</param>
+		/// <returns>The <see cref="System.String"/> contents of the embedded resource.</returns>
+		/// <remarks>
+		/// <para>
+		/// This method is handy in implementations of <see cref="CR_Documentor.Transformation.TransformEngine.GetHtmlPageTemplate"/>.
+		/// </para>
+		/// </remarks>
+		/// <exception cref="System.ArgumentNullException">
+		/// Thrown if <paramref name="assembly" /> or <paramref name="resourcePath" /> are <see langword="null" />.
+		/// </exception>
+		/// <exception cref="System.ArgumentException">
+		/// Thrown if <paramref name="resourcePath" /> is <see cref="System.String.Empty"/>
+		/// or if it is not found in the assembly.
+		/// </exception>
+		public static string ReadEmbeddedResourceString(Assembly assembly, string resourcePath)
+		{
+			if (assembly == null)
+			{
+				throw new ArgumentNullException("assembly");
+			}
+			if (resourcePath == null)
+			{
+				throw new ArgumentNullException("resourcePath");
+			}
+			if (resourcePath == "")
+			{
+				throw new ArgumentException("Path to embedded resource to read may not be empty.", "resourcePath");
+			}
+
+			using (Stream stm = assembly.GetManifestResourceStream(resourcePath))
+			{
+				if (stm == null)
+				{
+					throw new ArgumentException("The resource path was not found in the assembly.", "resourcePath");
+				}
+				using (StreamReader sreader = new StreamReader(stm))
+				{
+					string retVal = sreader.ReadToEnd();
+					return retVal;
+				}
+			}
+		}
+	}
+}
---
+++ branches/issue/20/tags/source/mergepoint00/Plugin/Server/HttpRequestEventArgs.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net;
+
+namespace CR_Documentor.Server
+{
+	/// <summary>
+	/// Event arguments for responding to incoming HTTP requests.
+	/// </summary>
+	public class HttpRequestEventArgs : EventArgs
+	{
+		/// <summary>
+		/// Gets the incoming request context information.
+		/// </summary>
+		/// <value>
+		/// A <see cref="System.Net.HttpListenerContext"/> with the incoming
+		/// request information and the stream on which to respond to the
+		/// request.
+		/// </value>
+		public HttpListenerContext RequestContext { get; private set; }
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="HttpRequestEventArgs"/> class.
+		/// </summary>
+		/// <param name="requestContext">The incoming HTTP request context.</param>
+		public HttpRequestEventArgs(HttpListenerContext requestContext)
+		{
+			this.RequestContext = requestContext;
+		}
+	}
+}
