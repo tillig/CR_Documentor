58653c77034d232f853903d29a2532d478677b5f
travis.illig <travis.illig@e29d1091-9d51-0410-bb39-710e510d8bfb>
2010-01-29 19:53:14 +0000

    Resolved issue #22: Added ability for actions previously only accessible via context menu to also be available through assignable hotkeys. These actions are now available in the DevExpress Options menu under IDE/Shortcuts. * Collapse XML Doc Comments - Collapses the XML documentation comment blocks in a source file. * Expand XML Doc Comments - Expands the XML documentation comment blocks in a source file. * Toggle CR_Documentor Window - Shows/hides the CR_Documentor tool window. * XML Encode Selection - Encodes special XML characters in the active selection for inclusion in XML. * Convert Selection to XML Doc Comment - Converts the active whole line (single or multiple lines) selection into an XML doc comment block.

--- trunk/Plugin/ContextMenu/Button/ConvertSelectionToCommentButton.cs
+++ trunk/Plugin/ContextMenu/Button/ConvertSelectionToCommentButton.cs
@@ -1,7 +1,5 @@
 using System;
-using System.Text.RegularExpressions;
 using CR_Documentor.Diagnostics;
-using DevExpress.CodeRush.Core;
 
 namespace CR_Documentor.ContextMenu.Button
 {
@@ -22,8 +20,7 @@ namespace CR_Documentor.ContextMenu.Button
 		{
 			get
 			{
-				if (!CodeRush.Context.Satisfied(DXCoreContext.CTX_SelectionWholeLine, true) &&
-					!CodeRush.Context.Satisfied(DXCoreContext.CTX_SelectionMultiLine, true))
+				if (!DXCoreContext.HasActiveWholeOrMultiLineSelection)
 				{
 					return false;
 				}
@@ -36,87 +33,7 @@ namespace CR_Documentor.ContextMenu.Button
 		/// </summary>
 		public override void Execute()
 		{
-			using (ActivityContext context = new ActivityContext(Log, "Converting selected text to XML doc comment."))
-			{
-				try
-				{
-					// Get the active text document
-					if (TextView.Active == null)
-					{
-						// The active text document wasn't retrievable.
-						Log.Write(LogLevel.Warn, "Unable to retrieve active text document.");
-						return;
-					}
-
-					if (!TextView.Active.Selection.Exists)
-					{
-						// There has to be a selection to convert
-						Log.Write(LogLevel.Warn, "No selection to convert.");
-						return;
-					}
-
-					// Mark the undo stack
-					CodeRush.UndoStack.BeginUpdate("ConvertSelectionToComment");
-
-					// Get the full content to insert
-					Log.Write(LogLevel.Info, "XML encoding selected text.");
-					TextView.Active.Selection.ExtendToWholeLines();
-					TextView.Active.Selection.Text = this.XmlEncode(TextView.Active.Selection.Text);
-
-					// Tabify the selection, figure out how many tabs are at the start of
-					// each line and calculate the minimum.  Remove that many tabs from
-					// each line.
-					Log.Write(LogLevel.Info, "Tabifying insertion.");
-
-					// The TextView.Active.Selection.Tabify() call doesn't work because
-					// it always tabifies the NEXT LINE AFTER THE END. We have to do
-					// it manually.
-					for (int i = TextView.Active.Selection.StartLine; i < TextView.Active.Selection.EndLine; i++)
-					{
-						TextView.Active.Selection.TextDocument.TabifyLine(i);
-					}
-					string[] lines = TextView.Active.Selection.Lines;
-					int minTabs = Int32.MaxValue;
-					foreach (string line in lines)
-					{
-						Match tabMatch = Regex.Match(line, @"^(\t*)[^\t]", RegexOptions.None);
-						if (tabMatch.Success && tabMatch.Groups[1].Captures[0].Length < minTabs)
-						{
-							minTabs = tabMatch.Groups[1].Captures[0].Length;
-						}
-					}
-					for (int i = 0; i < minTabs; i++)
-					{
-						TextView.Active.Selection.Unindent();
-					}
-
-					// Add the XML Doc Comment prefix to each line
-					Log.Write(LogLevel.Info, "Adding XML doc comment prefix to each line.");
-					string prefix = DocumentorContextMenu.CommentPrefix + " ";
-					TextView.Active.Selection.Text = Regex.Replace(TextView.Active.Selection.Text, "^", prefix, RegexOptions.Multiline);
-					// Remove the one at the end because it will accidentally change the line after the selection into a doc comment.
-					TextView.Active.Selection.Text = Regex.Replace(TextView.Active.Selection.Text, prefix + "$", "", RegexOptions.Singleline);
-
-					// Formatting will re-indent the new comment into the proper location
-					// using correct spaces/tabs based on user settings.
-					Log.Write(LogLevel.Info, "Formatting the new XML doc comment.");
-					TextView.Active.Selection.Format();
-
-					// Finish with the undo stack
-					CodeRush.UndoStack.EndUpdate();
-
-					// Move to the end of the insertion
-					Log.Write(LogLevel.Info, "Positioning caret.");
-					CodeRush.Caret.MoveTo(TextView.Active.Selection.EndSourcePoint);
-
-					Log.Write(LogLevel.Info, "Comment conversion complete.");
-				}
-				catch (Exception err)
-				{
-					Log.Write(LogLevel.Error, "Error while converting text.", err);
-				}
-			}
+			DocumentorActions.ConvertSelectionToXmlDocComment();
 		}
-
 	}
 }
--- trunk/Plugin/ContextMenu/Button/XmlEncodeButton.cs
+++ trunk/Plugin/ContextMenu/Button/XmlEncodeButton.cs
@@ -22,7 +22,7 @@ namespace CR_Documentor.ContextMenu.Button
 		{
 			get
 			{
-				if (!CodeRush.Selection.Exists)
+				if (!DXCoreContext.HasActiveSelection)
 				{
 					return false;
 				}
@@ -35,57 +35,7 @@ namespace CR_Documentor.ContextMenu.Button
 		/// </summary>
 		public override void Execute()
 		{
-			using (ActivityContext context = new ActivityContext(Log, "XML encoding selected text."))
-			{
-				try
-				{
-					// Get the active text document
-					TextDocument activeTextDocument = CodeRush.Documents.ActiveTextDocument;
-					if (activeTextDocument == null)
-					{
-						// The active text document wasn't retrievable.
-						Log.Write(LogLevel.Warn, "Unable to retrieve active text document.");
-						return;
-					}
-
-					if (!CodeRush.Selection.Exists)
-					{
-						// There has to be a selection to XML encode
-						Log.Write(LogLevel.Info, "No selection to encode.");
-						return;
-					}
-
-					// Get the full content to insert
-					Log.Write(LogLevel.Info, "XML encoding selected text.");
-					string replacement = this.XmlEncode(CodeRush.Selection.Text);
-
-					// Insert the content (replace any selected text)
-					CodeRush.UndoStack.BeginUpdate("XmlEncodeSelection");
-					Log.Write(LogLevel.Info, "Deleting original text.");
-					CodeRush.Selection.Delete();
-					Log.Write(LogLevel.Info, "Inserting XML encoded text.");
-					SourceRange insertedRange = activeTextDocument.InsertText(CodeRush.Caret.SourcePoint, replacement);
-					CodeRush.UndoStack.EndUpdate();
-
-					// Move to the end of the insertion
-					CodeRush.Caret.MoveTo(insertedRange.Bottom);
-					Log.Write(LogLevel.Info, "XML encode complete.");
-				}
-				catch (Exception err)
-				{
-					Log.Write(LogLevel.Error, "Error happened while XML encoding text.", err);
-				}
-			}
-		}
-
-		/// <summary>
-		/// XML encodes a string.
-		/// </summary>
-		/// <param name="toEncode">The string to encode.</param>
-		/// <returns>The string with XML entities encoded.</returns>
-		protected virtual string XmlEncode(string toEncode)
-		{
-			return toEncode.Replace("&", "&amp;").Replace("<", "&lt;").Replace(">", "&gt;").Replace("\"", "&quot;");
+			DocumentorActions.XmlEncodeSelection();
 		}
 	}
 }
