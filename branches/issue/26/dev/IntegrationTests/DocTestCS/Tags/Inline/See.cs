8dd192a8dfb0615b04924a20f52cfcfe198f6a0e
travis.illig <travis.illig@e29d1091-9d51-0410-bb39-710e510d8bfb>
2010-03-09 01:00:12 +0000

    Added integration test for see langword.

--- branches/issue/26/dev/IntegrationTests/DocTestCS/Tags/Inline/See.cs
+++ branches/issue/26/dev/IntegrationTests/DocTestCS/Tags/Inline/See.cs
@@ -8,6 +8,24 @@ namespace DocTestCS.Tags.Inline
 	public class SeeClass
 	{
 		/// <summary>
+		/// This is a method that tests the see langword functionality.
+		/// </summary>
+		/// <remarks>
+		/// <list type="bullet">
+		/// <item><term>null:  <see langword="null"/></term></item>
+		/// <item><term>sealed:  <see langword="sealed"/></term></item>
+		/// <item><term>static:  <see langword="static"/></term></item>
+		/// <item><term>abstract:  <see langword="abstract"/></term></item>
+		/// <item><term>virtual:  <see langword="virtual"/></term></item>
+		/// <item><term>true:  <see langword="true"/></term></item>
+		/// <item><term>false:  <see langword="false"/></term></item>
+		/// </list>
+		/// </remarks>
+		public void Langword()
+		{
+		}
+
+		/// <summary>
 		/// This is a method with a see that has a cref: <see cref="System.String" />.
 		/// </summary>
 		public void MethodCref()
