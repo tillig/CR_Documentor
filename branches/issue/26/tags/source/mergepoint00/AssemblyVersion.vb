bc4e844ceb6dfa41f0b7f10608cfc516be575ac8
travis.illig <travis.illig@e29d1091-9d51-0410-bb39-710e510d8bfb>
2010-03-11 16:26:06 +0000

    Created mergepoint for bringing trunk changes into task.

---
+++ branches/issue/26/tags/source/mergepoint00/AssemblyVersion.vb
@@ -0,0 +1,6 @@
+Imports System
+Imports System.Reflection
+
+<Assembly: AssemblyProduct("CR_Documentor")> 
+<Assembly: AssemblyCopyright("Copyright © 2004 CR_Documentor Contributors")> 
+<Assembly: AssemblyVersion("2.4.0.0")> 
