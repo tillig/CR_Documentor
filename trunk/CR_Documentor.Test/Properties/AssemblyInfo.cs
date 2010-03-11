b62f60adbf5fdcffe40e62e18bbb74a941ba85fd
travis.illig <travis.illig@e29d1091-9d51-0410-bb39-710e510d8bfb>
2010-03-11 16:32:40 +0000

    Resolved issue #26: Fixed some incorrect context menu behavior in VS2010 by adjusting how selections are calculated. Some minor issues remain but are DXCore issues. Upgraded solution to VS2010 format.

--- trunk/CR_Documentor.Test/Properties/AssemblyInfo.cs
+++ trunk/CR_Documentor.Test/Properties/AssemblyInfo.cs
@@ -3,7 +3,6 @@ using System.Runtime.InteropServices;
 using System.Security.Permissions;
 
 [assembly: ComVisible(false)]
-[assembly: SecurityPermission(SecurityAction.RequestMinimum)]
 
 [assembly: AssemblyTitle("CR_Documentor Tests")]
 [assembly: AssemblyDescription("Unit tests for the CR_Documentor product.")]
