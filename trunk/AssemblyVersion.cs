0d6a54820f57c6ad0dee52a2f357643ae6fd174d
travis.illig <travis.illig@e29d1091-9d51-0410-bb39-710e510d8bfb>
2010-03-12 18:22:05 +0000

    Updated version to 2.4.1.0.

--- trunk/AssemblyVersion.cs
+++ trunk/AssemblyVersion.cs
@@ -3,4 +3,4 @@ using System.Reflection;
 
 [assembly: AssemblyProduct("CR_Documentor")]
 [assembly: AssemblyCopyright("Copyright © 2004 CR_Documentor Contributors")]
-[assembly: AssemblyVersion("2.4.0.0")]
+[assembly: AssemblyVersion("2.4.1.0")]
