24a15464d6be77f2a58f8f9880ff564d1316176b
travis.illig <travis.illig@e29d1091-9d51-0410-bb39-710e510d8bfb>
2010-06-21 15:16:00 +0000

    Created branchpoint for issue 30 codeline.

---
+++ branches/issue/30/tags/source/branchpoint/IntegrationTests/DocTestVB/My Project/AssemblyInfo.vb
@@ -0,0 +1,7 @@
+Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+<Assembly: AssemblyTitle("DocTest VB")> 
+<Assembly: AssemblyDescription("VB code for testing the XML doc comment preview.")> 
+<Assembly: ComVisible(False)>
