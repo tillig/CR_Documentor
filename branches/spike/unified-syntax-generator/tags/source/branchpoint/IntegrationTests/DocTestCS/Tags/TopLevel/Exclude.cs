91245a5b0dd0761bff00e053e31e9a1a8ae9bc54
travis.illig <travis.illig@e29d1091-9d51-0410-bb39-710e510d8bfb>
2010-06-23 18:14:53 +0000

    Created branchpoint for spike unified-syntax-generator codeline.

---
+++ branches/spike/unified-syntax-generator/tags/source/branchpoint/IntegrationTests/DocTestCS/Tags/TopLevel/Exclude.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace DocTestCS.Tags.TopLevel
+{
+	/// <summary>
+	/// This class tests the 'exclude' tag.
+	/// </summary>
+	/// <exclude />
+	public class ExcludeClass
+	{
+		/// <summary>
+		/// This is a method with an exclude.
+		/// </summary>
+		/// <exclude />
+		public void Method()
+		{
+		}
+	}
+	/// <summary>
+	/// This interface tests the 'exclude' tag.
+	/// </summary>
+	/// <exclude />
+	public interface ExcludeInterface
+	{
+		/// <summary>
+		/// This is a method inside an excluded interface.
+		/// </summary>
+		void Method();
+	}
+}
