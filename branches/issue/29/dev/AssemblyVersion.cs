1043fa97714e572a2c215149f36f78e6ccb81b45
travis.illig <travis.illig@e29d1091-9d51-0410-bb39-710e510d8bfb>
2010-06-23 18:07:39 +0000

    Removing folder for issue 29. Work on this will be deferred until we have a unified syntax generator.

--- branches/issue/29/dev/AssemblyVersion.cs
+++
@@ -1,6 +0,0 @@
-using System;
-using System.Reflection;
-
-[assembly: AssemblyProduct("CR_Documentor")]
-[assembly: AssemblyCopyright("Copyright © 2004 CR_Documentor Contributors")]
-[assembly: AssemblyVersion("2.4.1.0")]
--- branches/issue/29/dev/CR_Documentor.Test/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-using System.Security.Permissions;
-
-[assembly: ComVisible(false)]
-
-[assembly: AssemblyTitle("CR_Documentor Tests")]
-[assembly: AssemblyDescription("Unit tests for the CR_Documentor product.")]
