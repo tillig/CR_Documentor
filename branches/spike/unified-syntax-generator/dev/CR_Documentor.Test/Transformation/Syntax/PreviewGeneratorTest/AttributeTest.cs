924c57903079dd05742c34d6c7617839e397af94
travis.illig <travis.illig@e29d1091-9d51-0410-bb39-710e510d8bfb>
2010-06-30 15:53:33 +0000

    Refactoring tests into more functionality-based fixtures to avoid duplicating tests.

---
+++ branches/spike/unified-syntax-generator/dev/CR_Documentor.Test/Transformation/Syntax/PreviewGeneratorTest/AttributeTest.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using CR_Documentor.Test.Transformation.Syntax.PreviewGeneratorTest.Proxies;
+using CR_Documentor.Transformation.Syntax;
+using DevExpress.CodeRush.StructuralParser;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TypeMock.ArrangeActAssert;
+
+namespace CR_Documentor.Test.Transformation.Syntax.PreviewGeneratorTest
+{
+	[TestClass]
+	[Isolated]
+	public class AttributeTest
+	{
+		[TestMethod]
+		public void Class_Single_Basic()
+		{
+			ClassProxy info = new ClassProxy("TestClass");
+			AttributeProxy attrib = new AttributeProxy("SerializableAttribute");
+			info.Attributes.Add(attrib.CreateFakeAttribute());
+			var element = info.CreateFakeClass();
+
+			string expected =
+@"<div class=""code vb"">
+<div class=""attributes"">
+<div class=""attribute"">
+&lt;<a href=""#"">SerializableAttribute</a>&gt; _
+</div>
+</div><div class=""member"">
+<span class=""keyword"">Public</span> <span class=""keyword"">Class</span> <span class=""identifier"">TestClass</span>
+</div>
+</div>";
+
+			var generator = new PreviewGenerator(element, SupportedLanguageId.Basic);
+			string actual = generator.Generate();
+			Assert.AreEqual(expected, actual);
+		}
+
+		[TestMethod]
+		public void Class_Single_CSharp()
+		{
+			ClassProxy info = new ClassProxy("TestClass");
+			AttributeProxy attrib = new AttributeProxy("SerializableAttribute");
+			info.Attributes.Add(attrib.CreateFakeAttribute());
+			var element = info.CreateFakeClass();
+
+			string expected =
+@"<div class=""code cs"">
+<div class=""attributes"">
+<div class=""attribute"">
+[<a href=""#"">SerializableAttribute</a>]
+</div>
+</div><div class=""member"">
+<span class=""keyword"">public</span> <span class=""keyword"">class</span> <span class=""identifier"">TestClass</span>
+</div>
+</div>";
+
+			var generator = new PreviewGenerator(element, SupportedLanguageId.CSharp);
+			string actual = generator.Generate();
+			Assert.AreEqual(expected, actual);
+		}
+
+		[TestMethod]
+		public void Class_Multiple_Basic()
+		{
+			ClassProxy info = new ClassProxy("TestClass");
+			AttributeProxy attrib = new AttributeProxy("SerializableAttribute");
+			info.Attributes.Add(attrib.CreateFakeAttribute());
+			attrib = new AttributeProxy("XmlRootAttribute");
+			attrib.Arguments.Add(new ExpressionProxy("\"root\"").CreateFakeExpression());
+			attrib.Arguments.Add(new BinaryOperatorExpressionProxy("DataType", "\"string\"").CreateFakeExpression());
+			info.Attributes.Add(attrib.CreateFakeAttribute());
+			var element = info.CreateFakeClass();
+
+			string expected =
+@"<div class=""code vb"">
+<div class=""attributes"">
+<div class=""attribute"">
+&lt;<a href=""#"">SerializableAttribute</a>&gt; _
+</div><div class=""attribute"">
+&lt;<a href=""#"">XmlRootAttribute</a><div class=""parameters"">
+(<div class=""parameter"">
+<span class=""literal"">&quot;root&quot;</span>, 
+</div><div class=""parameter"">
+<span class=""identifier"">DataType</span> := <span class=""literal"">&quot;string&quot;</span>
+</div>)
+</div>&gt; _
+</div>
+</div><div class=""member"">
+<span class=""keyword"">Public</span> <span class=""keyword"">Class</span> <span class=""identifier"">TestClass</span>
+</div>
+</div>";
+
+			var generator = new PreviewGenerator(element, SupportedLanguageId.Basic);
+			string actual = generator.Generate();
+			Assert.AreEqual(expected, actual);
+		}
+
+		[TestMethod]
+		public void Class_Multiple_CSharp()
+		{
+			ClassProxy info = new ClassProxy("TestClass");
+			AttributeProxy attrib = new AttributeProxy("SerializableAttribute");
+			info.Attributes.Add(attrib.CreateFakeAttribute());
+			attrib = new AttributeProxy("XmlRootAttribute");
+			attrib.Arguments.Add(new ExpressionProxy("\"root\"").CreateFakeExpression());
+			attrib.Arguments.Add(new BinaryOperatorExpressionProxy("DataType", "\"string\"").CreateFakeExpression());
+			info.Attributes.Add(attrib.CreateFakeAttribute());
+			var element = info.CreateFakeClass();
+
+			string expected =
+@"<div class=""code cs"">
+<div class=""attributes"">
+<div class=""attribute"">
+[<a href=""#"">SerializableAttribute</a>]
+</div><div class=""attribute"">
+[<a href=""#"">XmlRootAttribute</a><div class=""parameters"">
+(<div class=""parameter"">
+<span class=""literal"">&quot;root&quot;</span>, 
+</div><div class=""parameter"">
+<span class=""identifier"">DataType</span> = <span class=""literal"">&quot;string&quot;</span>
+</div>)
+</div>]
+</div>
+</div><div class=""member"">
+<span class=""keyword"">public</span> <span class=""keyword"">class</span> <span class=""identifier"">TestClass</span>
+</div>
+</div>";
+
+			var generator = new PreviewGenerator(element, SupportedLanguageId.CSharp);
+			string actual = generator.Generate();
+			Assert.AreEqual(expected, actual);
+		}
+	}
+}
---
+++ branches/spike/unified-syntax-generator/dev/CR_Documentor.Test/Transformation/Syntax/PreviewGeneratorTest/ParamTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using CR_Documentor.Test.Transformation.Syntax.PreviewGeneratorTest.Proxies;
+using CR_Documentor.Transformation.Syntax;
+using DevExpress.CodeRush.StructuralParser;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TypeMock.ArrangeActAssert;
+
+namespace CR_Documentor.Test.Transformation.Syntax.PreviewGeneratorTest
+{
+	[TestClass]
+	[Isolated]
+	public class ParamTest
+	{
+	}
+}
