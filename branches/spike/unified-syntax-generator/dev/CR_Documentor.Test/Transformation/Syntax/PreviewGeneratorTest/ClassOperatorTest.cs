1e72ee0abb91a7c0f7ce67b9e6e5cde16cc81f9c
travis.illig <travis.illig@e29d1091-9d51-0410-bb39-710e510d8bfb>
2010-07-20 15:42:46 +0000

    Testing explicit casting operators.

--- branches/spike/unified-syntax-generator/dev/CR_Documentor.Test/Transformation/Syntax/PreviewGeneratorTest/ClassOperatorTest.cs
+++ branches/spike/unified-syntax-generator/dev/CR_Documentor.Test/Transformation/Syntax/PreviewGeneratorTest/ClassOperatorTest.cs
@@ -11,7 +11,72 @@ namespace CR_Documentor.Test.Transformation.Syntax.PreviewGeneratorTest
 	[Isolated]
 	public class ClassOperatorTest
 	{
-		// TODO: Test Explicit casting
+		[TestMethod]
+		public void ExplicitCast_Basic()
+		{
+			MethodProxy info = new MethodProxy("op_Implicit")
+			{
+				MemberType = "Integer",
+				IsStatic = true,
+				IsClassOperator = true,
+				IsExplicitCast = true
+			};
+			var param = new ParamProxy("param1")
+			{
+				ParamType = "TestClass"
+			};
+			info.Parameters.Add(param.CreateFakeParam());
+			var element = info.CreateFakeMethod();
+
+			string expected =
+@"<div class=""code vb"">
+<div class=""member"">
+<span class=""keyword"">Public</span> <span class=""keyword"">Shared</span> <span class=""keyword"">Narrowing</span> <span class=""keyword"">Operator</span> <span class=""identifier"">CType</span><div class=""parameters"">
+( _<div class=""parameter"">
+<span class=""identifier"">param1</span> <span class=""keyword"">As</span> <a href=""#"">TestClass</a> _
+</div>)
+</div> <span class=""keyword"">As</span> <a href=""#"">Integer</a>
+</div>
+</div>";
+
+			var generator = new PreviewGenerator(element, SupportedLanguageId.Basic);
+			string actual = generator.Generate();
+			Assert.AreEqual(expected, actual);
+		}
+
+		[TestMethod]
+		public void ExplicitCast_CSharp()
+		{
+			MethodProxy info = new MethodProxy("op_Implicit")
+			{
+				MemberType = "int",
+				IsStatic = true,
+				IsClassOperator = true,
+				IsExplicitCast = true
+			};
+			var param = new ParamProxy("param1")
+			{
+				ParamType = "TestClass"
+			};
+			info.Parameters.Add(param.CreateFakeParam());
+			var element = info.CreateFakeMethod();
+
+			string expected =
+@"<div class=""code cs"">
+<div class=""member"">
+<span class=""keyword"">public</span> <span class=""keyword"">static</span> <span class=""keyword"">explicit</span> <span class=""keyword"">operator</span> <a href=""#"">int</a><div class=""parameters"">
+(<div class=""parameter"">
+<a href=""#"">TestClass</a> <span class=""identifier"">param1</span>
+</div>)
+</div>
+</div>
+</div>";
+
+			var generator = new PreviewGenerator(element, SupportedLanguageId.CSharp);
+			string actual = generator.Generate();
+			Assert.AreEqual(expected, actual);
+		}
+
 		[TestMethod]
 		public void ImplicitCast_Basic()
 		{
