55af678043e40acc1610ee4a45ec596073bc5bde
travis.illig <travis.illig@e29d1091-9d51-0410-bb39-710e510d8bfb>
2010-08-09 15:42:55 +0000

    Created codeline for issue 28.

---
+++ branches/issue/28/dev/CR_Documentor.Test/Transformation/Syntax/PreviewGeneratorTest/Proxies/StructProxy.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using DevExpress.CodeRush.StructuralParser;
+using TypeMock.ArrangeActAssert;
+
+namespace CR_Documentor.Test.Transformation.Syntax.PreviewGeneratorTest.Proxies
+{
+	public class StructProxy : ClassProxy
+	{
+		public StructProxy(string name)
+			: base(name)
+		{
+		}
+
+		public Struct CreateFakeStruct()
+		{
+			var element = Isolate.Fake.Instance<Struct>();
+			Isolate.Swap.CallsOn(element).WithCallsTo(this);
+			return element;
+		}
+	}
+}
---
+++ branches/issue/28/dev/CR_Documentor.Test/Transformation/Syntax/PreviewGeneratorTest/Proxies/TypeParameterProxy.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using DevExpress.CodeRush.StructuralParser;
+using TypeMock.ArrangeActAssert;
+
+namespace CR_Documentor.Test.Transformation.Syntax.PreviewGeneratorTest.Proxies
+{
+	public class TypeParameterProxy
+	{
+		public string Name { get; private set; }
+		public TypeParameterConstraintCollection Constraints { get; set; }
+
+		public TypeParameterProxy(string name)
+		{
+			this.Name = name;
+			this.Constraints = new TypeParameterConstraintCollection();
+		}
+
+		public TypeParameter CreateFakeTypeParameter()
+		{
+			var element = Isolate.Fake.Instance<TypeParameter>();
+			Isolate.Swap.CallsOn(element).WithCallsTo(this);
+			return element;
+		}
+	}
+}
