fbcadc3a8286da6af5126d3b17b8bb5ac0011a95
travis.illig <travis.illig@e29d1091-9d51-0410-bb39-710e510d8bfb>
2010-09-14 15:34:25 +0000

    Created codeline for issue 4.

---
+++ branches/issue/4/dev/IntegrationTests/DocTestVB/Syntax/TestInterfaceImplementation.vb
@@ -0,0 +1,27 @@
+﻿Namespace Syntax
+	''' <summary>
+	''' This class is a test for the VB "Implements" keyword. It implements
+	''' the TestInterface.
+	''' </summary>
+	Public Class TestInterfaceImplementation
+		Implements TestInterface
+
+		''' <summary>
+		''' This is a member of the interface that is a Sub.
+		''' </summary>
+		Public Sub SubInterfaceMember() Implements TestInterface.SubInterfaceMember
+
+		End Sub
+
+		''' <summary>
+		''' This is a member of the interface that is a Function.
+		''' </summary>
+		''' <param name="param1">The first parameter.</param>
+		''' <returns>An arbitrary integer return value.</returns>
+		Public Function FunctionInterfaceMember(ByVal param1 As String) As Integer Implements TestInterface.FunctionInterfaceMember
+			Return 0
+		End Function
+
+	End Class
+End Namespace
+
---
+++ branches/issue/4/dev/Plugin/Transformation/Syntax/ParameterWriter.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Web.UI;
+using DevExpress.CodeRush.StructuralParser;
+
+namespace CR_Documentor.Transformation.Syntax
+{
+	/// <summary>
+	/// Writes parameter syntax previews.
+	/// </summary>
+	public static class ParameterWriter
+	{
+		/// <summary>
+		/// Writes parameter information to the object signature.
+		/// </summary>
+		/// <param name="writer">
+		/// The <see cref="System.Web.UI.HtmlTextWriter"/> to which the preview
+		/// is being written.
+		/// </param>
+		/// <param name="element">
+		/// The element for which parameters are being written.
+		/// </param>
+		/// <param name="language">
+		/// The langauge for which parametes are being written.
+		/// </param>
+		/// <param name="openParen">The open parenthesis (usually "(" but sometimes "[").</param>
+		/// <param name="closeParen">The close parenthesis (usually ")" but sometimes "]").</param>
+		public static void Write(HtmlTextWriter writer, MemberWithParameters element, SupportedLanguageId language, string openParen, string closeParen)
+		{
+			if (element == null)
+			{
+				return;
+			}
+			var parameters = element.Parameters;
+			if (parameters == null)
+			{
+				return;
+			}
+			int count = parameters.Count;
+			bool isBasic = language == SupportedLanguageId.Basic;
+			if (isBasic && count == 0)
+			{
+				// If there aren't any parameters in VB, no parens get
+				// rendered or anything.
+				return;
+			}
+
+			writer.AddAttribute(HtmlTextWriterAttribute.Class, PreviewCss.Parameters);
+			writer.RenderBeginTag(HtmlTextWriterTag.Div);
+			writer.Write(openParen);
+			if (isBasic)
+			{
+				writer.Write(" _");
+			}
+			for (int i = 0; i < count; i++)
+			{
+				var parameter = (Param)parameters[i];
+				writer.AddAttribute(HtmlTextWriterAttribute.Class, PreviewCss.Parameter);
+				writer.RenderBeginTag(HtmlTextWriterTag.Div);
+
+				// Sandcastle does not render anything around optional parameters
+				// but if that changes, we'll need to do something here.
+				//if (isBasic && parameter.IsOptional)
+				//{
+				//    HtmlTextWriterExtensions.WriteSpan(CssClassKeyword, "Optional");
+				//}
+
+				if (parameter.IsOutParam)
+				{
+					switch (language)
+					{
+						case SupportedLanguageId.Basic:
+							HtmlTextWriterExtensions.WriteLink(writer, "OutAttribute", "&lt;", "&gt; ");
+							HtmlTextWriterExtensions.WriteSpan(writer, PreviewCss.Keyword, "ByRef");
+							break;
+						default:
+							HtmlTextWriterExtensions.WriteSpan(writer, PreviewCss.Keyword, "out");
+							break;
+					}
+				}
+				else if (parameter.IsReferenceParam)
+				{
+					switch (language)
+					{
+						case SupportedLanguageId.Basic:
+							HtmlTextWriterExtensions.WriteSpan(writer, PreviewCss.Keyword, "ByRef");
+							break;
+						default:
+							HtmlTextWriterExtensions.WriteSpan(writer, PreviewCss.Keyword, "ref");
+							break;
+					}
+				}
+
+				if (parameter.IsParamArray)
+				{
+					switch (language)
+					{
+						case SupportedLanguageId.Basic:
+							HtmlTextWriterExtensions.WriteSpan(writer, PreviewCss.Keyword, "ParamArray");
+							break;
+						default:
+							HtmlTextWriterExtensions.WriteSpan(writer, PreviewCss.Keyword, "params");
+							break;
+					}
+				}
+
+				if (isBasic)
+				{
+					HtmlTextWriterExtensions.WriteSpan(writer, PreviewCss.Identifier, parameter.Name);
+					HtmlTextWriterExtensions.WriteSpan(writer, PreviewCss.Keyword, "As");
+				}
+				HtmlTextWriterExtensions.WriteLink(writer, parameter.ParamType, "", "");
+				if (!isBasic)
+				{
+					writer.Write(" ");
+					HtmlTextWriterExtensions.WriteSpan(writer, PreviewCss.Identifier, parameter.Name, "", "");
+				}
+
+				// Sandcastle doesn't render the default value settings, but if
+				// that changes, we'll have to put the default value here.
+				//if (isBasic && parameter.IsOptional)
+				//{
+				//    this.Writer.Write(" = ");
+				//    this.Writer.Write(parameter.DefaultValue);
+				//}
+
+				if (i + 1 < count)
+				{
+					writer.Write(",");
+				}
+				if (isBasic)
+				{
+					writer.Write(" _");
+				}
+				writer.RenderEndTag();
+
+			}
+
+			writer.Write(closeParen);
+			writer.RenderEndTag();
+		}
+	}
+}
