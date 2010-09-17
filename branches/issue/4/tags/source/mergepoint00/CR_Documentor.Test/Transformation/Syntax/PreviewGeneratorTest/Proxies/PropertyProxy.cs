bfd819e4ddfba9fa867d3fd1a825ca448dea0e07
travis.illig <travis.illig@e29d1091-9d51-0410-bb39-710e510d8bfb>
2010-09-17 20:20:52 +0000

    Created mergepoint for getting trunk changes into task.

---
+++ branches/issue/4/tags/source/mergepoint00/CR_Documentor.Test/Transformation/Syntax/PreviewGeneratorTest/Proxies/PropertyProxy.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using DevExpress.CodeRush.StructuralParser;
+using TypeMock.ArrangeActAssert;
+
+namespace CR_Documentor.Test.Transformation.Syntax.PreviewGeneratorTest.Proxies
+{
+	public class PropertyProxy : MemberWithParametersProxy
+	{
+		public bool HasGetter { get; set; }
+		public bool HasSetter { get; set; }
+
+		public PropertyProxy(string name)
+		{
+			this.Name = name;
+		}
+
+		public Property CreateFakeProperty()
+		{
+			var element = Isolate.Fake.Instance<Property>();
+			Isolate.Swap.CallsOn(element).WithCallsTo(this);
+			return element;
+		}
+	}
+}
