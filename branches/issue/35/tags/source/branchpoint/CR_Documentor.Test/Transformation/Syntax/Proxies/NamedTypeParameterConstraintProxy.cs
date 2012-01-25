2d374dd79812397563bbbe6253dbd20c1d6a8cab
travis.illig@gmail.com <travis.illig@gmail.com@e29d1091-9d51-0410-bb39-710e510d8bfb>
2012-01-25 17:13:55 +0000

    Created branchpoint for issue 35 codeline.

---
+++ branches/issue/35/tags/source/branchpoint/CR_Documentor.Test/Transformation/Syntax/Proxies/NamedTypeParameterConstraintProxy.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using DevExpress.CodeRush.StructuralParser;
+using TypeMock.ArrangeActAssert;
+
+namespace CR_Documentor.Test.Transformation.Syntax.Proxies
+{
+	public class NamedTypeParameterConstraintProxy
+	{
+		public TypeReferenceExpression TypeReference { get; private set; }
+
+		public NamedTypeParameterConstraintProxy(string stringValue)
+		{
+			this.TypeReference = new TypeReferenceExpressionProxy(stringValue).CreateFakeTypeReferenceExpression();
+		}
+
+		public NamedTypeParameterConstraint CreateFakeTypeParameter()
+		{
+			var element = Isolate.Fake.Instance<NamedTypeParameterConstraint>();
+			Isolate.Swap.CallsOn(element).WithCallsTo(this);
+			return element;
+		}
+	}
+}
---
+++ branches/issue/35/tags/source/branchpoint/CR_Documentor.Test/Transformation/Syntax/Proxies/TypeParameterConstraintProxy.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using DevExpress.CodeRush.StructuralParser;
+using TypeMock.ArrangeActAssert;
+
+namespace CR_Documentor.Test.Transformation.Syntax.Proxies
+{
+	public class TypeParameterConstraintProxy
+	{
+		public string Name { get; private set; }
+
+		public TypeParameterConstraintProxy(string name)
+		{
+			this.Name = name;
+		}
+
+		public TypeParameterConstraint CreateFakeTypeParameter()
+		{
+			var element = Isolate.Fake.Instance<TypeParameterConstraint>();
+			Isolate.Swap.CallsOn(element).WithCallsTo(this);
+			return element;
+		}
+	}
+}
