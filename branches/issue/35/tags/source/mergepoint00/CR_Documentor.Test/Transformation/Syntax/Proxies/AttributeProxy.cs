f3fdaa7beb1271f6c2ba12cc41fb2a76875cabcd
travis.illig@gmail.com <travis.illig@gmail.com@e29d1091-9d51-0410-bb39-710e510d8bfb>
2012-02-14 22:28:31 +0000

    Created tag for updating task.

---
+++ branches/issue/35/tags/source/mergepoint00/CR_Documentor.Test/Transformation/Syntax/Proxies/AttributeProxy.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using DevExpress.CodeRush.StructuralParser;
+using TypeMock.ArrangeActAssert;
+
+namespace CR_Documentor.Test.Transformation.Syntax.Proxies
+{
+	public class AttributeProxy
+	{
+		public int ArgumentCount
+		{
+			get
+			{
+				return this.Arguments.Count;
+			}
+		}
+
+		public ExpressionCollection Arguments { get; set; }
+		public string Name { get; set; }
+
+		public AttributeProxy(string name)
+		{
+			this.Name = name;
+			this.Arguments = new ExpressionCollection();
+		}
+
+		public DevExpress.CodeRush.StructuralParser.Attribute CreateFakeAttribute()
+		{
+			var element = Isolate.Fake.Instance<DevExpress.CodeRush.StructuralParser.Attribute>();
+			Isolate.Swap.CallsOn(element).WithCallsTo(this);
+			return element;
+		}
+	}
+}
---
+++ branches/issue/35/tags/source/mergepoint00/Plugin/Transformation/Syntax/SupportedLanguageId.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CR_Documentor.Transformation.Syntax
+{
+	/// <summary>
+	/// Enumerates the list of supported languages that can be used to generate
+	/// a syntax preview.
+	/// </summary>
+	public enum SupportedLanguageId
+	{
+		/// <summary>
+		/// No language, or a language that isn't otherwise supported.
+		/// </summary>
+		None,
+		
+		/// <summary>
+		/// C#
+		/// </summary>
+		CSharp,
+		
+		/// <summary>
+		/// Visual Basic
+		/// </summary>
+		Basic
+	}
+}
