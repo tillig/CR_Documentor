fc22334a8c1d9868558952bcdd174f3195f50952
travis.illig@gmail.com <travis.illig@gmail.com@e29d1091-9d51-0410-bb39-710e510d8bfb>
2012-08-16 18:03:15 +0000

    Updated project for VS2012. Includes version update to 3.1.0.1. Unit tests currently excluded from build due to Typemock installer issue on Windows 8.

--- AssemblyVersion.vb
+++ AssemblyVersion.vb
@@ -3,4 +3,4 @@ Imports System.Reflection
 
 <Assembly: AssemblyProduct("CR_Documentor")> 
 <Assembly: AssemblyCopyright("Copyright © 2004 CR_Documentor Contributors")> 
-<Assembly: AssemblyVersion("3.1.0.0")> 
+<Assembly: AssemblyVersion("3.1.0.1")> 
