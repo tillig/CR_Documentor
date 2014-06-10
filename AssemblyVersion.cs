625ec34b38658d0d59a81e1011ae9af7e804366f
travis.illig@gmail.com <travis.illig@gmail.com@e29d1091-9d51-0410-bb39-710e510d8bfb>
2014-06-10 16:59:05 +0000

    Updated security attributes to be compatible with .NET 4.0 and DXCore 14.1.

--- AssemblyVersion.cs
+++ AssemblyVersion.cs
@@ -3,4 +3,4 @@ using System.Reflection;
 
 [assembly: AssemblyProduct("CR_Documentor")]
 [assembly: AssemblyCopyright("Copyright © 2004 CR_Documentor Contributors")]
-[assembly: AssemblyVersion("3.1.0.2")]
+[assembly: AssemblyVersion("3.1.1.0")]
