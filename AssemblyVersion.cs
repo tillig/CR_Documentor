1ec22be7444129b8c4b4d5c50d8697c68c3c07c1
Travis Illig <tillig@paraesthesia.com>
2015-07-22 11:41:27 -0700

    Fixed assembly reference issues. Incremented version.

--- AssemblyVersion.cs
+++ AssemblyVersion.cs
@@ -3,4 +3,4 @@ using System.Reflection;
 
 [assembly: AssemblyProduct("CR_Documentor")]
 [assembly: AssemblyCopyright("Copyright © 2004 CR_Documentor Contributors")]
-[assembly: AssemblyVersion("4.0.0.0")]
+[assembly: AssemblyVersion("4.0.1.0")]
--- AssemblyVersion.vb
+++ AssemblyVersion.vb
@@ -3,4 +3,4 @@ Imports System.Reflection
 
 <Assembly: AssemblyProduct("CR_Documentor")>
 <Assembly: AssemblyCopyright("Copyright © 2004 CR_Documentor Contributors")>
-<Assembly: AssemblyVersion("4.0.0.0")>
+<Assembly: AssemblyVersion("4.0.1.0")>
